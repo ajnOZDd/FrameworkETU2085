7c0ab29b46da5697751055c7ad502d6b546e2fc3
allan <107546913+ajnOZDd@users.noreply.github.com>
2023-07-02 22:35:50 +0300

    Add files via upload

---
+++ test-framework/script.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set TOMCAT_DIR=D:\fianarana\xampp\tomcat
+set WEBAPPS_DIR=%TOMCAT_DIR%\webapps
+set WAR_NAME=test-framework
+
+echo Creating the WAR file...
+jar cvf %WAR_NAME%.war -C %WEBAPPS_DIR%\test-framework .
+
+echo Deploying the WAR file to Tomcat...
+
+move %WAR_NAME%.war %TOMCAT_DIR%\webapps
+
+if %errorlevel% neq 0 (
+    echo Failed to deploy the WAR file.
+) else (
+    echo WAR file deployed successfully.
+)
